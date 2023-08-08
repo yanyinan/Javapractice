1f45f2c21e9714b782042576b74474cccc64fa3e
Nanzhou <2648179906@qq.com>
2023-08-08 22:00:13 +0800

    2023年08月08日

--- Individual_exercises/src/Main2.java
+++ Individual_exercises/src/Main2.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
  * @author:nanzhou
  * @date:
  */
+/*
+ 四舍五入
+ 描述
+        定义一个int类型变量i,i为由浮点数变量d四舍五入后的整数类型，请将转换后的i进行输出
+        输入描述：
+        用户随机输入的浮点数
+        输出描述：
+        四舍五入之后的整数（小数点后一位>=5则进一，否则舍去）*/
 public class Main2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

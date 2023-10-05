190f84da9e17a4fa79dc9d17390a499d3565fb88
Nanzhou <2648179906@qq.com>
2023-10-05 15:00:12 +0800

    2023年10月05日

--- Test_the_project/src/ann/test.java
+++ Test_the_project/src/ann/test.java
@@ -1,11 +1,13 @@
 package ann;
 
+import java.lang.annotation.Documented;
+
 /**
  * @title:
  * @author:nanzhou
  * @date:
  */
-
+@Documented
 public class Test {
     @TestAnnotation(id = 1,msg = "ss")
     public void f(){}

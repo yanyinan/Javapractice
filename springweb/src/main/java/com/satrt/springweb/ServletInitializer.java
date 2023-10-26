244756f883805fb255ae732ecc59adff49211b67
Nanzhou <2648179906@qq.com>
2023-10-26 10:00:12 +0800

    2023年10月26日

--- springweb/src/main/java/com/satrt/springweb/ServletInitializer.java
+++ springweb/src/main/java/com/satrt/springweb/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.satrt.springweb;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override

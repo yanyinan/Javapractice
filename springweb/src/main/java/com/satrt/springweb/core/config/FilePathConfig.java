692cffa79060e9e793d58559bdcdad0507a64b95
Nanzhou <2648179906@qq.com>
2023-10-27 10:00:14 +0800

    2023年10月27日

--- springweb/src/main/java/com/satrt/springweb/core/config/FilePathConfig.java
+++ springweb/src/main/java/com/satrt/springweb/core/config/FilePathConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * 配置文件路径
  * @author 26481
@@ -14,9 +17,10 @@ public class FilePathConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadPath = Paths.get(Constant.UPLOAD_PATH).toAbsolutePath();
         //和页面有关的静态目录都放在项目的static目录下
         registry.addResourceHandler("/web/**")
-                .addResourceLocations("file:" + Constant.UPLOAD_PATH);
+                .addResourceLocations("file:" + uploadPath);
     }
 
 

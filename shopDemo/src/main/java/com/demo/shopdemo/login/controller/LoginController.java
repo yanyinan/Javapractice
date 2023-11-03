587588669baaa893bbd8f8376726aded31728689
Nanzhou <2648179906@qq.com>
2023-11-03 18:00:12 +0800

    2023年11月03日

---
+++ shopDemo/src/main/java/com/demo/shopdemo/login/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.demo.shopdemo.login.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户登录
+ *
+ * @author: Nanzhou
+ * @version: v0.0.1
+ * @Date: 2023/11/3
+ */
+@RestController
+public class LoginController {
+}

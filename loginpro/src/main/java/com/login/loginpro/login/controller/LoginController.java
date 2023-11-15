18e8c4e1d6ac841eec0ee9b726f52e630d326814
Nanzhou <2648179906@qq.com>
2023-11-15 15:00:13 +0800

    2023年11月15日

--- loginpro/src/main/java/com/login/loginpro/login/controller/LoginController.java
+++ loginpro/src/main/java/com/login/loginpro/login/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.login.loginpro.login.controller;
 
 import com.login.loginpro.core.model.UserLogin;
+import com.login.loginpro.login.service.ILoginService;
+import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  * @project: loginpro
  * @title: LoginController （默认）
@@ -17,6 +19,14 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class LoginController {
+    /**
+     * ioc 构造器注入
+     */
+    private ILoginService loginService;
+    public LoginController(ILoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @GetMapping({"/", "/login"})
     public ModelAndView login() {
         return new ModelAndView("/login");
@@ -25,7 +35,16 @@ public class LoginController {
     @PostMapping("/login")
     public ModelAndView login(UserLogin userLogin) {
         ModelAndView mv = new ModelAndView("login");
+        if(userLogin.getLid() != null){
+            if (userLogin.getPassword() != null){
+
+            }
+        }
 
         return mv;
+    }
+    @GetMapping("/captcha")
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        CaptchaUtil.out(request, response);
     }
 }
